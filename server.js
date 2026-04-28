0f65f4405f2c7a688bea49fe71cc638f974571a3
unknown <emmanuelkingchris@gmail.com>
2026-04-29 00:36:25 +0100

    Fix mongoose duplicate decleration

--- server.js
+++ server.js
@@ -17,8 +17,6 @@ app.use(cors({
 }));
 app.use(bodyParser.json());
 
-const mongoose = require("mongoose");
-
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
